2e32b4190a65851890aa83b7d3965066fad22be5
dotkalm <joelmholmberg@gmail.com>
2019-07-26 12:55:52 -0700

    adds post controller

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,1 +1,18 @@
-//
+const Post = require('../models/posts');
+const User = require('../models/users')
+const mongoose = require('mongoose');
+
+router.get('/'), async (req,res) => {
+    try{
+        const foundPosts = await Post.find({}).populate('author');
+        console.log(foundPosts, '<--------')
+        res.render('photos/index.ejs', {
+            posts: foundPosts
+        })
+    } catch(err){
+        res.send(err)
+    }
+
+}
+    
+module.exports = router;
