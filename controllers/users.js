5a24d6680c4dede0bac1f8339b5d00f0b8ff073b
CT <ct777@CTs-MacBook-Pro.local>
2019-07-26 12:55:11 -0700

    set up login route on usercontroller

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,41 @@
 const express   = require('express');
 const router    = express.Router();
 const User      = require('../models/users');
+const bcrypt    = require('bcryptjs');
 
+router.post('login', async (req, res) =>{
+
+    try {
+        const foundUser = await User.findOne({username: req.body.username});
+        console.log(foundUser, ' foundUser');
+
+
+    if(foundUser){
+
+        if(bcrypt.compareSync(req.body.password, foundUser.password)){
+            req.session.userID = foundUser._id;
+            req.session.username = foundUser.username;
+            req.session.logged = true;
+
+            res.redirect('/models')
+
+        } else {
+
+            req.session.message = "Username or Password incorrect";
+            res.redirect('/');
+        }
+
+        } else {
+            req.session.message = "Username or Password incorrect";
+            res.redirect('/')
+    }
+
+    } catch(err){
+        res.send(err);
+    }
+
+});
+
+
+
+//router.post('/register', async (req))
