1f172d97a88523b06b9a312dceb8eb62282e10aa
CT <ct777@CTs-MacBook-Pro.local>
2019-07-26 14:40:34 -0700

    added server.js file

---
+++ server.js
@@ -0,0 +1,37 @@
+const express           = require('express');
+const bodyParer         = require('body-parser');
+const methodOverride    = require('method-override');
+const session           = require('express-session');
+const app               = express();
+
+
+require('./db/db');
+// require our controller, Which is the router object
+const commentsController    = require('./controllers/comments');
+const postsController       = require('./controllers/posts');
+const usersController       = require('./controllers/users');
+
+
+app.use(session({
+    secret: 'SUE!, WHERE ARE YOU?!',
+    resave: false,
+    saveUninitialized: false
+
+}));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
+app.use('/comments', commentsController);
+app.use('/posts', postsController);
+app.use('/users', usersController);
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {
+        message: req.session.message
+    });
+});
+
+app.listen(8080, () => {
+    console.log('listening on port 8080');
+});
