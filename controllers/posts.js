6bbcf7c1eb85af255ee476323159e05065e8e525
dotkalm <joelmholmberg@gmail.com>
2019-07-26 16:40:28 -0700

    adds posts index route

--- controllers/posts.js
+++ controllers/posts.js
@@ -4,17 +4,18 @@ const Post = require('../models/posts');
 const User = require('../models/users')
 
 
-router.get('/'), async (req,res) => {
+router.get('/', async (req,res) => {
+    
     try{
         const foundPosts = await Post.find({}).populate('author');
         console.log(foundPosts, '<--------')
-        res.render('photos/index.ejs', {
+        res.render('posts/index.ejs', {
             posts: foundPosts
         })
     } catch(err){
         res.send(err)
     }
 
-}
+})
     
 module.exports = router;
