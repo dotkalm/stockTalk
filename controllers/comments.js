9d0a1f17c5c29ad4164fe15a88d6ecbd53c4e798
CT <ct777@CTs-MacBook-Pro.local>
2019-07-29 16:10:43 -0700

    started creation of comments routes

--- controllers/comments.js
+++ controllers/comments.js
@@ -2,5 +2,29 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/posts');
 const User = require('../models/users');
+const Comment = require('../models/comments')
+
+router.get('/', (req, res) =>{
+    console.log(req.session, ' req.session in index or article')
+    Comment.find({}, (err, found))
+});
+
+router.post('/', (req, res)=>{
+    Comment.create(req.body, (err, createdComment)=>{
+        if(err){
+            res.send(err);
+        } else {
+            console.log(req.body)
+            Post.findById(req.body.postId, (err, foundPosts) =>{
+                console.log(foundPosts, '<--- foundPost in comment create route')
+                foundPosts.comments.push(createdComment);
+                foundPosts.save((err, savedComment ) => {
+                    console.log(savedPost, 'this is savedPost');
+                    res.redirect('/posts');
+                });
+            });
+        }
+    });
+});
 
 module.exports = router
