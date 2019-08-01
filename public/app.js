6a73bd5c5815cde90856d579b4ee586fc9c809b8
dotkalm <joelmholmberg@gmail.com>
2019-07-31 20:59:59 -0700

    header nav

--- public/app.js
+++ public/app.js
@@ -3,8 +3,9 @@ console.log('I SEE YOU IN THE CONSOLE')
 const h1selector = document.querySelector('h1')
 h1selector.classList.add("animate");
 h1selector.setAttribute('style', `animation-delay: .1s`)
-// for (let i=0; i<h1selector.innerText.length; i++){
-//         h1selector.innerText[i].setAttribute(`animation-delay: ${i}s`)
-// }
 
-console.log()
+const logOut = document.querySelector('.logOut')
+console.log(logOut.innerText)
+logOut.onclick = () => {
+    
+  }
